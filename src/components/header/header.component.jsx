2d64a0c4bd951d97369bd9a057fe89c6bee27180
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-06 14:15:45 -0600

    created signin page with sign in component and added more styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
         <Logo className="logo" />
       </Link>
       <div className="options">
-        <Link to='/shop' className="option">SHOP</Link>
-        <Link to='/shop' className="option">CONTACT</Link> 
+        <Link to="/shop" className="option">
+          SHOP
+        </Link>
+        <Link to="/shop" className="option">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            Sign Out
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
