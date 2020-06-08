57735d6b1e961297246c1d0133f871c85e05fa44
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-07 18:33:49 -0600

    moved shop data to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,7 +4,8 @@ import storage from "redux-persist/lib/storage"; // local storage
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-import directoryReducer from './directory/directory.reducer';
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
   key: "root",
@@ -14,8 +15,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer, 
-  directory: directoryReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
-export default persistReducer(persistConfig, rootReducer)
+export default persistReducer(persistConfig, rootReducer);
