5a0593f4cd2eb773f1a23a9182a1dbccf1d8a78f
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-08 00:57:07 -0600

    implemented stripe payment api

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 
 import "./checkout.styles.scss";
 
-const CheckoutPage = ({cartItems, total}) => {
+const CheckoutPage = ({ cartItems, total }) => {
   return (
     <div className="checkout-page">
       <div className="checkout-header">
@@ -28,22 +33,25 @@ const CheckoutPage = ({cartItems, total}) => {
           <span>Remove</span>
         </div>
       </div>
-      {
-        cartItems.map(cartItem => (
-          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-        ))
-      }
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
 
       <div className="total">
-    <span>TOTAL: ${total}</span>
+        <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">*Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/2022 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems, 
-  total: selectCartTotal
-})
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
 
 export default connect(mapStateToProps)(CheckoutPage);
