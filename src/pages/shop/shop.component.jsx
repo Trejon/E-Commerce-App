4e6beae5ddb9af8ca3cb55074960708b5cde7418
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-08 14:38:33 -0600

    added spinner to display when app is loading data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -12,7 +12,16 @@ import {
 
 import { updateCollections } from "../../redux/shop/shop.actions.js";
 
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionsPageWithSpinner = WithSpinner(CollectionsPage)
+
 class ShopPage extends React.Component {
+  state = { 
+    loading: true,
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -22,17 +31,19 @@ class ShopPage extends React.Component {
     collectionRef.onSnapshot(async (snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}`} render={(props) =>(<CollectionsOverviewWithSpinner isLoading={loading} {...props} />)} />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionsPage}
+          render={(props) =>(<CollectionsPageWithSpinner isLoading={loading} {...props} />)}
         />
       </div>
     );
