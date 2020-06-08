64901f7f66a1c2782ee24a180f2f5881fba5d9b4
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-08 13:55:47 -0600

    moved shop data to firebase store

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -21,3 +21,15 @@ const CustomButton = ({
 };
 
 export default CustomButton;
+
+
+
+// import React from 'react';
+
+// import { CustomButtonContainer } from './custom-button.styles';
+
+// const CustomButton = ({ children, ...props }) => (
+//   <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+// );
+
+// export default CustomButton;
