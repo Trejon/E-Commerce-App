428542542f46aedfaa25937171494be62a0e5502
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-08 00:12:31 -0600

    added collection routes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,14 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionsPage from '../collection/collection.component';
 
-import { selectCollections } from "../../redux/shop/shop.selectors";
-
-const ShopPage = ({ collections }) => (
-  <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionsPage} />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -4,11 +4,11 @@ const INITIAL_STATE = {
   collections: SHOP_DATA
 };
 
-const shopReducer = (state=INITIAL_STATE, action) => {
-  switch(action.type) {
-    default: 
+const shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
       return state;
   }
-}
+};
 
 export default shopReducer;
