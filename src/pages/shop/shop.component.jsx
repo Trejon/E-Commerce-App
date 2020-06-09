25a0edd88ad89647bfacb7ef3ec9dd159ebfb202
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-08 20:01:40 -0600

    added HOC collectionContainer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { createStructuredSelector } from 'reselect';
 import { connect } from "react-redux";
 
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
-import CollectionsPage from "../collection/collection.component";
+import CollectionsPageContainer from "../collection/collection.container";
 
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions.js";
-import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors'
 
-import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
-const CollectionsPageWithSpinner = WithSpinner(CollectionsPage)
 
 class ShopPage extends React.Component {
   componentDidMount() {
@@ -20,25 +16,21 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionLoaded } = this.props;
+    const { match } = this.props;
     return (
       <div className="shop-page">
         <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
         <Route 
           path={`${match.path}/:collectionId`}
-          render={(props) =>(<CollectionsPageWithSpinner isLoading={!isCollectionLoaded} {...props} />)}
+          component={CollectionsPageContainer}
         />
       </div>
     );
   }
 }
 
-const mapStateToProps = createStructuredSelector({ 
-  isCollectionLoaded: selectIsCollectionsLoaded
-});
-
 // const mapDispatchToProps = dispatch => ({
 //   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 // });
 
-export default connect(mapStateToProps, { fetchCollectionsStartAsync })(ShopPage);
+export default connect(null, { fetchCollectionsStartAsync })(ShopPage);
