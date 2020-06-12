6c824aabe19b6d4f9a4b2433b9b7a11b02d84728
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-06-12 15:20:13 -0600

    added styled components for each component and started making site mobile friendly

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-import CollectionItem from "../collection-item/collection-item.component";
-
-// import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 import {
   CollectionPreviewContainer,
   TitleContainer,
-  PreviewContainer,
-} from "./collection-preview.styles";
+  PreviewContainer
+} from './collection-preview.styles';
 
 const CollectionPreview = ({ title, items, history, match, routeName }) => (
   <CollectionPreviewContainer>
@@ -18,8 +16,8 @@ const CollectionPreview = ({ title, items, history, match, routeName }) => (
     </TitleContainer>
     <PreviewContainer>
       {items
-        .filter((item, index) => index < 4)
-        .map((item) => (
+        .filter((item, idx) => idx < 4)
+        .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
     </PreviewContainer>
--- client/src/components/sign-in/sign-in.component.jsx
+++ client/src/components/sign-in/sign-in.component.jsx
@@ -1,77 +1,84 @@
-import React, { useState } from "react";
-import { connect } from "react-redux";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
-import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-button.component";
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import {
   googleSignInStart,
-  emailSignInStart,
-} from "../../redux/user/user.actions";
+  emailSignInStart
+} from '../../redux/user/user.actions';
 
-import "./sign-in.styles.scss";
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer
+} from './sign-in.styles';
 
-const SignIn = ({ googleSignInStart, emailSignInStart }) => {
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
   const [userCredentials, setCredentials] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: ''
   });
 
   const { email, password } = userCredentials;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async event => {
+    event.preventDefault();
 
     emailSignInStart(email, password);
   };
 
-  const handleChange = (e) => {
-    const { value, name } = e.target;
+  const handleChange = event => {
+    const { value, name } = event.target;
 
     setCredentials({ ...userCredentials, [name]: value });
   };
 
   return (
-    <div className="sign-in">
-      <h2>I already have an account</h2>
+    <SignInContainer>
+      <SignInTitle>I already have an account</SignInTitle>
       <span>Sign in with your email and password</span>
 
       <form onSubmit={handleSubmit}>
         <FormInput
-          name="email"
-          type="email"
-          value={email}
-          label="email"
+          name='email'
+          type='email'
           handleChange={handleChange}
+          value={email}
+          label='email'
           required
         />
         <FormInput
-          name="password"
-          type="password"
+          name='password'
+          type='password'
           value={password}
-          label="password"
           handleChange={handleChange}
+          label='password'
           required
         />
-        <div className="buttons">
-          <CustomButton type="submit">Sign In</CustomButton>
+        <ButtonsBarContainer>
+          <CustomButton type='submit'> Sign in </CustomButton>
           <CustomButton
-            type="button"
+            type='button'
             onClick={googleSignInStart}
             isGoogleSignIn
           >
-            Sign In With Google
+            Sign in with Google
           </CustomButton>
-        </div>
+        </ButtonsBarContainer>
       </form>
-    </div>
+    </SignInContainer>
   );
 };
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   googleSignInStart: () => dispatch(googleSignInStart()),
   emailSignInStart: (email, password) =>
-    dispatch(emailSignInStart({ email, password })),
+    dispatch(emailSignInStart({ email, password }))
 });
 
-export default connect(null, mapDispatchToProps)(SignIn);
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignIn);
